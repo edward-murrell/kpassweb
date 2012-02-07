ffe7ec9dd13a8a10f9e04e41c23e11ea8fb8fb49
Edward Murrell <edward@murrell.co.nz>
2012-02-07 17:11:35 +1100

    Draft of config plans

--- conf/config.php
+++ conf/config.php
@@ -1,13 +1,17 @@
 <?php
 
-// Turn this into a singleton
+// TODO - Turn this into a singleton
 class kpassweb_config {
-	//$realm = 'LOCAL';
 }
 
 // Set the desired realm
-// $config->realm = 'MURRELL.CO.NZ';
+$config->addRealm ('EXAMPLE.COM','localhost');
+$config->addRealm ('MULTI.EXAMPLE.COM',array('10.1.1.1','kdc2.local'));
+// $config->addRealm ('MULTI.EXAMPLE.COM'); // configured via DNS / krb5.conf
 
+/* TODO - Add these methods;
+ * $config->getRealms() // returns array of valid realms
+ * $config->getKDC($realm);
 
 
 ?>
