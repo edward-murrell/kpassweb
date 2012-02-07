460c2ca143492927c1ce60e970ce2840b3a5b92c
Edward Murrell <edward@murrell.co.nz>
2012-02-08 10:37:56 +1100

    Added multirealm support to the config file

--- conf/config.php
+++ conf/config.php
@@ -1,12 +1,13 @@
 <?php
-
-// TODO - Turn this into a singleton
+/* 
+ *
+ */
 class kpassweb_config {
 	private static $instance;
-	private $realms;
+	protected $realms;
 
 	public function __construct() {
-		$this->realms = array(); 
+		$this->realms = array();
 	}
 
     public static function singleton()
@@ -16,26 +17,29 @@ class kpassweb_config {
         return self::$instance;
     }
 
-	public function addRealm($realm = null, $host = '127.0.0.1') {
+	public static function addRealm($realm = null, $host = '127.0.0.1') {
+		$conf = self::singleton();
 		// TODO - check for realm validity
 		// TODO - check for host validity
-		$this->realms[$realm] = $host;
+		$conf->realms[$realm] = $host;
 	}
-	public function getRealms() {
-		return array_keys($this->realms);
+	public static function getRealms() {
+		$conf = self::singleton();
+		return array_keys($conf->realms);
 	}
 	public function getKDC($realm) {
-		if (!array_key_exists($realm,$this->realms))
+		$conf = self::singleton();
+		if (!array_key_exists($realm,$conf->realms))
 			return false;
 		else
-			return $this->realms[$realm];
+			return $conf->realms[$realm];
 	}
 
 }
 
 // Set the desired realm
-$kpassweb_config->addRealm ('EXAMPLE.COM','localhost');
-$kpassweb_config->addRealm ('SUBDOMAIN.EXAMPLE.COM','localhost');
+kpassweb_config::addRealm ('EXAMPLE.COM','localhost');
+kpassweb_config::addRealm ('SUBDOMAIN.EXAMPLE.COM','localhost');
 
 /* $kpassweb_config->addRealm ('MULTI.EXAMPLE.COM',array('10.1.1.1','kdc2.local'));
  * $kpassweb_config->addRealm ('MULTI.EXAMPLE.COM'); // configured via DNS / krb5.conf
@@ -44,5 +48,4 @@ $kpassweb_config->addRealm ('SUBDOMAIN.EXAMPLE.COM','localhost');
  * $config->getRealms() // returns array of valid realms
  * $config->getKDC($realm);
  */
-
 ?>
